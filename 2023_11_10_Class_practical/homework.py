d4b4f706c1121f56e515a7285fdab430820f1ecf
crendo11 <crendo11@eafit.edu.co>
2023-11-19 17:40:49 +0100

    extracting features from several images

--- 2023_11_10_Class_practical/homework.py
+++ 2023_11_10_Class_practical/homework.py
@@ -11,19 +11,14 @@ sys.path.insert(0, './')
 
 import feature_detection as fd
 
-# load the image
-image = cv2.imread('./2023_11_10_Class_practical/images/1.jpg')
-# convert to gray scale image
-Iog = image.copy()
-I = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # define the size of the window
 size = 3
 
 # constants
-k = 0.05
+k = 0.04
 sigma = 0.5
-threshold = 0.005
+threshold = 0.5
 
 # create w matrix
 w = fd.generate_gaussian_kernel(size)
@@ -33,17 +28,50 @@ Gx = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
 
 Gy = Gx.T
 
-H = fd.get_harris_matrix(I, k, Gx, Gy, w)
-# define the radius of the circle based on the size of the image
-radius = int(I.shape[0]*0.005)
 
-corners = fd.get_harris_corners(H, threshold)
+# create array to store the list of features of each image
+features_list = []
+
+# iterate over the 10 images
+for z in range(10):
+
+    # load the image
+    image = cv2.imread('./2023_11_10_Class_practical/images/{}.jpg'.format(z))
+    # convert to gray scale image
+    Iog = image.copy()
+    I = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    H = fd.get_harris_matrix(I, k, Gx, Gy, w)
+    # define the radius of the circle based on the size of the image
+    radius = int(I.shape[0]*0.0025)
+
+    corners = fd.get_harris_corners(H, threshold)
+
+    features = fd.calculate_hog(I, corners)
+
+    # append the features to the list of features
+    features_list.append(features)
+
+    for corner in corners:
+        i, j = corner
+        cv2.circle(Iog, (i,j), radius, (0,255,0), 1)
+
+    # save the image
+    cv2.imwrite('./2023_11_10_Class_practical/images/{}_corners_t_5.jpg'.format(z), Iog)
+
+    # console status
+    print('Image {} processed.'.format(z))
+
+
+# write the features to a file so it can be used later
+with open('./2023_11_10_Class_practical/features_t_5.txt', 'w') as f:
+    for item in features_list:
+        f.write("%s\n" % item)
+
 
 # calculate the harris matrix
-for corner in corners:
-    i, j = corner
-    cv2.circle(Iog, (j,i), radius, (0,255,0), 1)
 
-# show the image with matplotlib
-plt.imshow(Iog)
-plt.show()
+
+# # show the image with matplotlib
+# plt.imshow(Iog)
+# plt.show()
---
+++ 2023_11_17_Class_practical/linear_classifier.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# define sigmoid function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return 2*np.exp(-x) * x / (1 + np.exp(-x))**2
+    
+
+def loss_function(x, y, theta):
+    m = y.shape[0]
+    return np.sum(y - sigmoid(np.dot(theta, x))**2)*1/m
+
+# gradient based on the mean squared error
+def gradient(x, y, theta):
+    m = y.shape[0]
+    return 1/m * np.sum(y - sigmoid(np.dot(theta, x)))*sigmoid_derivative(np.dot(theta, x))
+
+def gradient_decend(x, y, theta, alpha, iterations):
+    # initialize theta 
+    theta = np.zeros(x.shape[1])
+
+    while iterations > 0:
+        # update theta
+        theta = theta + alpha * gradient(x, y, theta)
+        iterations -= 1
+
+    return theta
+
+# create the data 
+x = np.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
+y = np.array([0,0,1,1,1])
+
+# plot the data
+plt.scatter(x[:,1], y)
+plt.show()
+# initialize theta
+theta = np.zeros(x.shape[1])
+
+# define the learning rate
+alpha = 0.01
+# define the number of iterations
+iterations = 100
+
+# compute the gradient decend
+theta = gradient_decend(x.T, y, theta, alpha, iterations)
+        
+# # plot the data
+# plt.figure()
+# plt.scatter(x[:,1], y)
+# plt.plot(x[:,1], sigmoid(np.dot(x, theta)))
+# plt.show()
+
+
