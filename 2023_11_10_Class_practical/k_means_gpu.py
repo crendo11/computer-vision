3468d7974111d0a8c13a2781787d714d7adcd0bc
crendo11 <crendo11@eafit.edu.co>
2023-11-20 15:17:33 +0100

    new k_mean setup and loaded

--- 2023_11_10_Class_practical/k_means_gpu.py
+++ 2023_11_10_Class_practical/k_means_gpu.py
@@ -8,11 +8,17 @@ from numba import jit
 
 
 # define distance function
-def distance(x,y):
-    # normalize vectors
-    x = x / np.linalg.norm(x)
-    y = y / np.linalg.norm(y)
-    return np.dot(x,y)
+@jit(nopython=True)
+def distance(x, y):
+    # calculate the norm of the vectors
+    x_norm = np.sqrt(np.sum(x ** 2))
+    y_norm = np.sqrt(np.sum(y ** 2))
+    # normalize the vectors
+    x = x / x_norm
+    y = y / y_norm
+    # calculate the dot product manually
+    dot_product = np.sum(x * y)
+    return dot_product
 
 def read_centers(filename):
     centers = np.loadtxt(filename, dtype=float)
@@ -56,6 +62,7 @@ centroids = read_centers('./2023_11_10_Class_practical/k_means.txt')
 # initialize the distances
 distances = np.ones((num_features, k))
 
+@jit(nopython=True)
 def k_means(features, centroids, labels, distances, iterations):
     loss = 1e10
     # iterate over the number of iterations or when the loss is 0
@@ -76,21 +83,25 @@ def k_means(features, centroids, labels, distances, iterations):
             # assign the label to the feature
             labels[i] = index
 
+        features_j = np.empty_like(features)  # pre-allocate features_j
         # iterate over the centroids
         for j in range(k):
-            # get the features with the label j 
-            features_j = features[labels == j]
-            # calculate the mean of the features
-            centroids[j] = np.mean(features_j, axis=0)
+            # get the features with the label j
+            indices = [i for i, x in enumerate(labels) if x == j]
+            for i in indices:
+                features_j[i] = features[i]
+            # calculate the mean of the features manually
+            centroids[j] = np.sum(features_j, axis=0) / features_j.shape[0]
 
 
         # update the number of iterations
         iterations -= 1
-        print('Loss: {}'.format(loss))
-        print('Iterations: {}'.format(iterations))
+        print('Loss: ' + str(loss))
+        print('Iterations: ' + str(iterations))
 
     return centroids, labels, distances, loss
 
+centroids = np.array(centroids, dtype=np.float64)
 centroids, labels, distances, loss = k_means(features, centroids, labels, distances, iterations)
 
 # write the k centroids to a file
