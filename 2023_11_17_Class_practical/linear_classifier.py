corrected gradient

2023_11_17_Class_practical/linear_classifier.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def sigmoid_derivative(x):
-    return 2*np.exp(-x) * x / (1 + np.exp(-x))**2
+def sigmoid_derivative(theta, x):
+    return 2*np.exp(np.dot(-theta, x)) * x / (1 + np.exp(np.dot(-theta,x)))**2
     
 
 def loss_function(x, y, theta):
@@ -16,28 +16,30 @@ def loss_function(x, y, theta):
 # gradient based on the mean squared error
 def gradient(x, y, theta):
     m = y.shape[0]
-    return 1/m * np.sum(y - sigmoid(np.dot(theta, x)))*sigmoid_derivative(np.dot(theta, x))
+    return 1/m * np.sum(y - sigmoid(np.dot(theta, x))*sigmoid_derivative(theta, x))
 
 def gradient_decend(x, y, theta, alpha, iterations):
-    # initialize theta 
-    theta = np.zeros(x.shape[1])
-
     while iterations > 0:
         # update theta
-        theta = theta + alpha * gradient(x, y, theta)
+        theta = theta + alpha * gradient(x.T, y, theta)
         iterations -= 1
 
     return theta
 
-# create the data 
-x = np.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
-y = np.array([0,0,1,1,1])
+# create the data two groups of points clearely clustered
+x = np.array([[1, 1], 
+              [1, 2], 
+              [2, 1], 
+              [2, 2], 
+              [4, 4], 
+              [4, 5], 
+              [5, 4], 
+              [5, 5]])
 
-# plot the data
-plt.scatter(x[:,1], y)
-plt.show()
+y = np.array([0, 0, 0, 0, 1, 1, 1, 1])
+
 # initialize theta
-theta = np.zeros(x.shape[1])
+theta = np.zeros(x.shape[1])     # the shape of theta is the number of columns of x
 
 # define the learning rate
 alpha = 0.01
@@ -45,7 +47,7 @@ alpha = 0.01
 iterations = 100
 
 # compute the gradient decend
-# theta = gradient_decend(x.T, y, theta, alpha, iterations)
+theta = gradient_decend(x, y, theta, alpha, iterations)
         
 # # plot the data
 plt.figure()
@@ -54,4 +56,10 @@ plt.scatter(x[:,1], y)
 # also plot the points
 plt.figure()
 plt.scatter(x[:,0], x[:,1])
+
+#plot the sigmoid function
+plt.figure()
+plt.plot(x[:,1], sigmoid(np.dot(theta, x.T)))
+
+
 plt.show()