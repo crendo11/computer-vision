9e4f1f1cf7616a80a84c3d67bed1c933d7e89492
crendo11 <crendo11@eafit.edu.co>
2023-12-04 00:03:58 +0100

    trying to fix linear classifier

--- 2023_11_17_Class_practical/linear_classifier.py
+++ 2023_11_17_Class_practical/linear_classifier.py
@@ -11,7 +11,7 @@ def sigmoid_derivative(theta, x):
 # gradient based on the mean squared error
 def gradient(x, y, theta):
     m = y.shape[0]
-    return 1/m * np.sum(y - sigmoid(np.dot(theta, x))*sigmoid_derivative(theta, x), axis=1)
+    return 1/m * np.sum((y - sigmoid(np.dot(theta, x)))*sigmoid_derivative(theta, x), axis=1)
 
 
 def gradient_decend(x, y, theta, alpha, iterations):
@@ -31,20 +31,13 @@ x = np.array([[1, 1],
               [4, 3], 
               [5, 2], 
               [5, 3],
-            #   [4+4, 3],
-            #   [4+4, 6],
-            #   [3+4, 4],
-            #   [3+4, 5],
-            #   [4+4, 2],
-            #   [4+4, 7],
-            #   [2+4, 4],
-            #   [2+4, 5]
             ])
 
 y = np.array([0, 0, 0, 0, 1, 1, 1, 1]) #, 1, 1, 1 , 1, 1, 1, 1, 1])
 
 # center the data
-
+x[:,0] = x[:,0] - np.mean(x[:,0])
+x[:,1] = x[:,1] - np.mean(x[:,1])
 
 # initialize theta
 theta = np.zeros(x.shape[1])     # the shape of theta is the number of columns of x
@@ -78,18 +71,21 @@ plt.grid()
 
 #plot the sigmoid function
 plt.figure()
-plt.plot(x[:,1], sigmoid(np.dot(theta, x.T)))
+plt.plot(range(len(x)), sigmoid(np.dot(theta, x.T)))
 
-# get a ramdom sample of points
-x_test = np.random.rand(100, 2)
+# get a ramdom sample of points 
+x_test = np.random.rand(100, 2) 
 
+# # add a translation
+# x_test[:,0] = x_test[:,0]*1.2 + 1
+# x_test[:,1] = x_test[:,1]*1.2 + 1
+# center the data
+x_test[:,0] = x_test[:,0] - np.mean(x_test[:,0])
+x_test[:,1] = x_test[:,1] - np.mean(x_test[:,1])
 
 # plot the points
 plt.figure()
 plt.scatter(x_test[:,0], x_test[:,1])
-
-# order the points by increasing order of x[1]
-x_test = x_test[np.argsort(x_test[:,1])]
 
 # apply the classifier
 y_test = sigmoid(np.dot(theta, x_test.T))
@@ -99,15 +95,14 @@ plt.figure()
 plt.scatter(x_test[:,0], x_test[:,1], c=y_test)
 plt.colorbar()
 # plot the line
-x_plot = np.linspace(-0.5, 0.5, 100)
-y_plot = m*x_plot + c
+x_plot = np.linspace(0, 1, 100)
+y_plot = m*x_plot
 plt.plot(x_plot, y_plot, 'r')
+plt.grid()
 
-# draw the sigmoid function
+#plot the sigmoid of the test points
 plt.figure()
-plt.plot(x_test[:,1], y_test)
-
-
+plt.plot(range(len(x_test)), y_test)
 
 
 plt.show()
